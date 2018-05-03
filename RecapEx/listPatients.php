3e3b9212afe623e141c8a4a7db6e4d79a7c8f375
elkom1 <elkom1@bfh.ch>
2018-05-03 16:52:19 +0200

    listpatients musab modifiziert

--- RecapEx/listPatients.php
+++ RecapEx/listPatients.php
@@ -1,3 +1,25 @@
+<html>
+<head>
+<style type="text/css">
+  body { background: yellow }
+  table, th, td { border: 1px solid black; border-collapse: collapse }
+  h1 {color: red}
+  </style>
+</head>
+
+<body>
+<h1>List of patients</h1>
+
+<table>
+<tr>
+    <th>patientID</th>
+    <th>MRN</th>
+    <th>name</th>
+    <th>first_name</th>
+    <th>gender</th>
+    <th>birthdate</th>
+</tr>
+
 <?php
 session_start();
 
@@ -12,23 +34,34 @@ include('pdo.inc.php');
 try {
     $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
     /*** echo a message saying we have connected ***/
-    echo '<h1>List of patients</h1>';
-    $sql = "select * from patient";
+
+    $sql = "SELECT * FROM patient";
 
     $result = $dbh->query($sql);
 
-    while($line = $result->fetch()){
-      echo "<a href='viewPatient.php?id=".$line['patientID']."'>";
-      echo $line['first_name']." ".$line['name'];
+    // while($line = $result->fetch()){
+    //   echo "<a href='viewPatient.php?id=".$line['patientID']."'>";
+    //   echo $line['first_name']." ".$line['name'];
 
-      echo "</a><br>\n";
+    //   echo "</a><br>\n";
+
+      foreach ($dbh -> query($sql) as $row) {
+        echo "<tr>
+          <td> $row[patientID].</td>
+          <td> $row[MRN].</td>
+          <td>
+          <a href=viewPatient.php?id=$row[patientID]>$row[name]</a>
+           .</td>
+          <td> $row[first_name].</td>
+          <td> $row[gender].</td>
+          <td> $row[birthdate].</td>
+        </tr>";
     }
 
     $dbh = null;
 }
 catch(PDOException $e)
 {
-
     /*** echo the sql statement and error message ***/
     echo $e->getMessage();
 }
