0221020cd7111f419411937c03e38397fd3fbeb0
Musab <musabgamal.elkour@students.bfh.ch>
2018-05-08 20:16:06 +0200

    musss

--- RecapEx/public_html/patientList.php
+++ RecapEx/public_html/patientList.php
@@ -15,11 +15,8 @@
 </div>
 
 <div class="navbar">
-  <ul class="menu">
-    <li><a>Home</a></li>
-    <li><a>Einstellungen</a></li>
-    <li><a href="logout.php" class="right">Logout</a></li>
-  </ul>
+    <a>Home</a>
+    <a href="logout.php" class="right">Logout</a>
 </div>
 
 <div class="row">
@@ -33,14 +30,13 @@
         exit();
         }
         include('pdo.inc.php');
-        echo "<a> User: ".$_SESSION['user']."</a>";
+        echo "<a> Welcome Dr. Marc ".$_SESSION['user']."</a>";
      ?>
 
       </div>
       <h2>Patient list</h2>
       <h5>Select Patient:</h5>
 
-      
       <table>
             <tr>
                 <th>Patient ID</th>
@@ -48,6 +44,7 @@
                 <th>First Name</th>
                 <th>Birthdate</th>
             </tr>
+
             <?php
             try {
                 $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
@@ -74,9 +71,59 @@
                 echo $e->getMessage();
             }
             ?> </table>
-      
+    
       <h3>Settings</h3>
-      <p>Add a patient</p>
+  <button class="button" id="addPatient"><i class="fas fa-user-plus"></i> New patient</button>
+  <div id="modalPatient" class="modal">
+      
+        <!-- Modal content -->
+        <div class="modal-content">
+          <span class="close">&times;</span>
+          <h2>Add patient</h2>
+          <form action="index.php" method="get">
+      <input type="hidden" name="id" value="">
+          <table>
+        <tr>
+            <td>Surname</td>
+          <td><input type="text" name="time" placeholder=""></td>
+        </tr>
+              <tr>
+                <td>First name</td>
+                  <td><input type="text" name="time" placeholder=""></td>
+              </tr>
+              <tr>
+                <td>Gender 1=male 2=female</td>
+                  <td><input type="text" name="gender" placeholder=""></td>
+              </tr>
+              <tr>
+                <td>Date of birth</td>
+                  <td><input type="text" name="time" placeholder="YYYY-MM-DD"></td>
+              </tr>
+          </table>
+          <input type="submit" value="Submit">
+      </form>
+        </div>
+      
+      </div>
+      
+  <script>
+    var modal = document.getElementById('modalPatient');
+    var btn = document.getElementById("addPatient");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  </script>
       
   </div>
 
